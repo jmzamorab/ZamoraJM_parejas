133682f2dce61f2c4d2314e47ae84e449f140174
jmzamorab <josem.zamorab@gmail.com>
2017-07-14 22:30:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/zamorajm_parejas/Casilla.java
@@ -0,0 +1,15 @@
+package com.example.zamorajm_parejas;
+
+import android.widget.Button;
+
+public class Casilla {
+    public int x;
+    public int y;
+    public Button boton;
+
+    public Casilla(Button boton, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.boton = boton;
+    }
+}
---
+++ app/src/main/java/com/example/zamorajm_parejas/Partida.java
@@ -0,0 +1,11 @@
+package com.example.zamorajm_parejas;
+
+public class Partida {
+    public static int FILAS = -1;
+    public static int COLUMNAS = -1;
+    public static int[][] casillas;
+    public static int turno;
+    public static int puntosJ1;
+    public static int puntosJ2;
+    public static String tipoPartida = "LOCAL";
+}
